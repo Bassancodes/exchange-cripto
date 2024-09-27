fad67aa472a8403fbacff3ba9622caabf8120bf5
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-09-27 18:35:37 -0300

    funcao de crirar usuario

--- funcoes.c
+++ funcoes.c
@@ -18,3 +18,31 @@ int login(Usuario usuarios[], int totalUsuarios, char cpf[], char senha[]) {
 }
 
 
+void criarUsuario(Usuario usuarios[], int *totalUsuarios) {
+    Usuario novoUsuario;
+
+    printf("Digite o CPF (11 digitos): ");
+    scanf("%s", novoUsuario.cpf);
+
+    for (int i = 0; i < *totalUsuarios; i++) {
+        if (strcmp(usuarios[i].cpf, novoUsuario.cpf) == 0) {
+            printf("Erro: Usuario com este CPF ja existe.\n");
+            return;
+        }
+    }
+
+    printf("Digite a senha: ");
+    scanf("%s", novoUsuario.senha);
+
+    novoUsuario.saldoReais = 0.0;
+    novoUsuario.saldoBitcoin = 0.0;
+    novoUsuario.saldoEthereum = 0.0;
+    novoUsuario.saldoRipple = 0.0;
+    novoUsuario.totalTransacoes = 0;
+
+    usuarios[*totalUsuarios] = novoUsuario;
+    (*totalUsuarios)++;
+
+    printf("Usuario criado com sucesso!\n");
+}
+
