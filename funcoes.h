a606628d9d5fcc8e69d5757ae37721c9b6da8d8a
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-09-27 18:26:48 -0300

    iniciando a funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,1 +1,14 @@
+//funcoes.h 
+#ifndef FUNCOES_H
+#define FUNCOES_H
 
+typedef struct {
+    char cpf[12];
+    char senha[20];
+    float saldoReais;
+    float saldoBitcoin;
+    float saldoEthereum;
+    float saldoRipple;
+    int totalTransacoes;
+    char historicoTransacoes[100][100];
+}
