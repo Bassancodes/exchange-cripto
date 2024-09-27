d3d013eaf66cfaee3151de0a61106f3dd9ca081e
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-09-27 18:07:24 -0300

    painel inicial

--- main.c
+++ main.c
@@ -9,3 +9,13 @@ int main() {
 
     int opcao, usuarioLogado = -1;
     char cpf[12], senha[20];
+
+ while (1) {
+        printf("\nCriptomoedas do Bassan\n");
+
+        if (usuarioLogado == -1) {
+            printf("1. Login\n");
+            printf("2. Criar novo usuario\n");
+            printf("0. Sair\n");
+            printf("Escolha uma opcao: ");
+            scanf("%d", &opcao);
