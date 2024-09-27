92c89e04bb9d80e46004f4336c44e7bd1abce0af
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-09-27 18:32:12 -0300

    iniciando a funcoes.c e o login

--- funcoes.c
+++ funcoes.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "funcoes.h"
+
+float cotacaoBitcoin = 100000.0;
+float cotacaoEthereum = 5000.0;
+float cotacaoRipple = 1.0;
+
+int login(Usuario usuarios[], int totalUsuarios, char cpf[], char senha[]) {
+    for (int i = 0; i < totalUsuarios; i++) {
+        if (strcmp(usuarios[i].cpf, cpf) == 0 && strcmp(usuarios[i].senha, senha) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
