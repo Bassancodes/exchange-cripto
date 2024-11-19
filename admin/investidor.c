ca0f36d5cf2e1351456c7a5c0f930c513ae22940
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-11-19 20:52:01 +0000

    Implementa a funcao de login no administrador

--- admin/investidor.c
+++ admin/investidor.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "admin.h"
+
+// Função para efetuar o login do administrador
+int efetuarLogin(char *cpf, char *senha) {
+    const char admin_cpf[] = "12345678901";
+    const char admin_senha[] = "admin123";
+
+    if (strcmp(cpf, admin_cpf) == 0 && strcmp(senha, admin_senha) == 0) {
+        return 1; // Login bem-sucedido
+    }
+    printf("CPF ou senha incorretos.\n");
+    return 0; // Falha no login
+}
 
