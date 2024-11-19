0bca2b27a98a71c880d0c9c43a2f378dfc024382
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-11-19 21:06:37 +0000

    funcao que exclui a cripto

--- admin/crypto.c
+++ admin/crypto.c
@@ -27,3 +27,39 @@ void cadastrarCriptomoeda() {
     printf("Criptomoeda cadastrada com sucesso!\n");
 }
 
+void excluirCriptomoeda() {
+    char nome[50];
+    printf("Digite o nome da criptomoeda a ser excluída: ");
+    scanf("%s", nome);
+
+    FILE *file = fopen("criptomoedas.dat", "r");
+    FILE *temp = fopen("temp.dat", "w");
+    if (file == NULL || temp == NULL) {
+        printf("Erro ao abrir os arquivos.\n");
+        return;
+    }
+
+    char nome_lido[50];
+    float cotacao, taxa_compra, taxa_venda;
+    int encontrado = 0;
+
+    while (fscanf(file, "%s %f %f %f\n", nome_lido, &cotacao, &taxa_compra, &taxa_venda) != EOF) {
+        if (strcmp(nome, nome_lido) == 0) {
+            encontrado = 1;
+            printf("Criptomoeda %s excluída.\n", nome);
+            continue;
+        }
+        fprintf(temp, "%s %.2f %.2f %.2f\n", nome_lido, cotacao, taxa_compra, taxa_venda);
+    }
+
+    fclose(file);
+    fclose(temp);
+
+    remove("criptomoedas.dat");
+    rename("temp.dat", "criptomoedas.dat");
+
+    if (!encontrado) {
+        printf("Criptomoeda %s não encontrada.\n", nome);
+    }
+}
+
