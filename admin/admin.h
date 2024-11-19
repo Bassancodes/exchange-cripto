ba89b8124d16d7af16389af7a64f92ba54873e65
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-11-19 20:42:23 +0000

    Funções para manipulação de criptomoedas e para manipulação de investidores

--- admin/admin.h
+++ admin/admin.h
@@ -1,1 +1,15 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+int efetuarLogin(char *cpf, char *senha);
+void cadastrarInvestidor();
+void excluirInvestidor();
+void consultarSaldoInvestidor();
+void consultarExtratoInvestidor();
+
+void cadastrarCriptomoeda();
+void excluirCriptomoeda();
+void atualizarCotacao();
+
+#endif
 
